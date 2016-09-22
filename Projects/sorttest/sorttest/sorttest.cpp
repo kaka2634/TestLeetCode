377bcaf21f35980d2dbd5459c3463590c0eeb4d4
liu <996529090@qq.com>
2016-09-23 00:25:22 +0300

    modified shellsort

--- Projects/sorttest/sorttest/sorttest.cpp
+++ Projects/sorttest/sorttest/sorttest.cpp
@@ -13,12 +13,30 @@ void insertsort(vector<Comparable>& a) // must use & for change a;
 	for(int i = 1;i < a.size(); i++)//i for 1;
 	{
 		Comparable tmp = a[i];
-		for (j = i; j > 0 && a[j-1] > tmp; j--)
+		for (j = i; j > 0 && a[j-1] > tmp; j=j-1)
 			a[j]=a[j-1];
 		a[j]=tmp;
 	}
-
 };
+
+template <typename Comparable>
+void shellsort(vector<Comparable>& a) // change insert minus 1 to minus gap!
+{
+	Comparable tmp;//dont forget tmp is Comparable!
+	int i,j;
+	for(int gap=a.size()/2;gap>0;gap/=2)
+	{
+		for (i = gap;i<a.size();i++) //i start from gap!
+		{
+			tmp = a[i];
+			j = i;
+			for (;j>=gap&&tmp<a[j-gap];j-=gap)//compare with gap!
+			a[j] = a[j-gap];
+		a[j]=tmp;
+		}
+	}
+}
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -28,7 +46,8 @@ int _tmain(int argc, _TCHAR* argv[])
 	vector<int> a;
 	while(fin>>k)
 		a.push_back(k);   
-	insertsort(a);
+	//insertsort(a);
+	shellsort(a);
 	for(auto v:a) fout<<v;
 	fin.close();
 	fout.close();
